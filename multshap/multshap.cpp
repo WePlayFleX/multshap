9d746ab3cf8f9a4b1b4309fa466fce544a6c62f2
Aliaksandr <alex.kamai@ya.ru>
2022-06-16 23:48:58 +0300

    V5

--- multshap/multshap.cpp
+++ multshap/multshap.cpp
@@ -1,11 +1,25 @@
 #include "msoftcon.h"
 
-class shape {
+class Shape {
 protected:
 	int xCo, yCo;
 	color fillcolor;
 	fstyle fillstyle;
 
 public:
+	Shape() : xCo(0), yCo(0), fillcolor(cWHITE), fillstyle(SOLID_FILL)
+	{
 
+	}
+
+	Shape(int x, int y, color fc, fstyle fs) : xCo(x), yCo(y), fillcolor(fc), fillstyle(fs)
+	{
+
+	}
+
+	void draw() const
+	{
+		set_color(fillcolor);
+		set_fill_style(fillstyle);
+	}
 };
