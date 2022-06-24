73925af474e3bf32f5d012309c0f5836022305a2
Aliaksandr <alex.kamai@ya.ru>
2022-06-24 23:35:40 +0300

    V10

--- multshap/multshap.cpp
+++ multshap/multshap.cpp
@@ -46,3 +46,69 @@ public:
 		draw_circle(xCo, yCo, radius);
 	}
 };
+
+class Rect : public Shape
+{
+private:
+	int width, height;
+
+public:
+	Rect() : Shape(), height(0), width(0)
+	{
+
+	}
+
+	Rect(int x, int y, int h, int w, color fc, fstyle fs) : Shape(x, y, fc, fs), height(h), width(w)
+	{
+
+	}
+
+	void draw() const
+	{
+		Shape::draw();
+		draw_rectangle(xCo, yCo, xCo + width, yCo + height);
+		set_color(cWHITE);
+		draw_line(xCo, yCo, xCo + width, yCo + height);
+	}
+};
+
+class Tria : public Shape
+{
+private:
+	int height;
+
+public:
+	Tria() : Shape(), height(0)
+	{
+
+	}
+
+	Tria(int x, int y, int h, color fc, fstyle fs) : Shape(x, y, fc, fs), height(h)
+	{
+
+	}
+
+	void draw()
+	{
+		Shape::draw();
+		draw_pyramid(xCo, yCo, height);
+	}
+};
+
+int main()
+{
+	init_graphics();
+
+	Circle cir(40, 12, 5, cBLUE, X_FILL);
+	Rect rec(12, 7, 10, 15, cRED, SOLID_FILL);
+	Tria tri(60, 7, 11, cGREEN, MEDIUM_FILL);
+
+	cir.draw();
+	rec.draw();
+	tri.draw();
+
+	set_cursor_pos(1, 25);
+
+	system("pause");
+	return 0;
+}
